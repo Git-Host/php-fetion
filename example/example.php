a535dcec314263e2356ac6895fe328d75d045f5f
phpqhz <phpqhz@c90e6b55-3540-7a16-0f74-cdbb131e2195>
2011-03-16 06:16:21 +0000

    PHPFetion介绍

--- example/example.php
+++ example/example.php
@@ -1,5 +1,5 @@
 <?php
 require '../lib/PHPFetion.class.php';
 
-fetion = new PHPFetion('13500001111', '123123');	// 手机号、飞信密码
-$result = $fetion->send('13500001111', 'Hello!');	// 接收人手机号、飞信内容
+$fetion = new PHPFetion('13500001111', '123123');	// 手机号、飞信密码
+$fetion->send('13500001111', 'Hello!');	// 接收人手机号、飞信内容
