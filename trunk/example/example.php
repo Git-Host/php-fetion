f99ae99b7d28590b01268d7e1081d739a02aa032
phpqhz <phpqhz@c90e6b55-3540-7a16-0f74-cdbb131e2195>
2011-06-04 07:11:18 +0000

    修改示例

--- trunk/example/example.php
+++ trunk/example/example.php
@@ -1,5 +1,8 @@
 <?php
 require '../lib/PHPFetion.php';
 
-$fetion = new PHPFetion('13500001111', '123123');	// 手机号、飞信密码
-$fetion->send('13500001111', 'Hello!');	// 接收人手机号、飞信内容
+$// 飞信手机号、飞信密码
+fetion = new PHPFetion('13500001111', '123123');
+
+// 接收人手机号、飞信内容
+echo $fetion->send('13500001111', 'Hello!'); 
