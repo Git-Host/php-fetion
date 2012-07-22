a21ce9a12e4497fffacc34297540780dbaa7ef2c
phpqhz <phpqhz@gmail.com>
2012-07-22 16:34:05 +0000

    给好友发信息时加上csrfToken字段，修复了不能给好友发信息的问题

--- example/example.php
+++ example/example.php
@@ -1,8 +1,5 @@
 <?php
 require '../lib/PHPFetion.php';
 
-$// 飞信手机号、飞信密码
-fetion = new PHPFetion('13500001111', '123123');
-
-// 接收人手机号、飞信内容
-echo $fetion->send('13500001111', 'Hello!'); 
+$fetion = new PHPFetion('13500001111', '123123');	// 手机号、飞信密码
+$fetion->send('13500001111', 'Hello!');	// 接收人手机号、飞信内容
